a081a52336fba81fcbaa812f418c843cfd57a83a
Ivan Pribela <ivanpribela@gmail.com>
2018-03-01 21:20:27 +0100

    Model, Klasa koja predstavlja vreme kao sat i minut

---
+++ src/org/svetovid/raspored/model/Vreme.java
@@ -0,0 +1,61 @@
+package org.svetovid.raspored.model;
+
+/**
+ * Ova klasa predstavlja vreme kao sat i minut.
+ *
+ * @author Ivan Pribela
+ */
+public final class Vreme implements Comparable<Vreme> {
+
+	private final int sat;
+	private final int minut;
+
+	public Vreme(int sat, int minut) throws IllegalArgumentException {
+		this.sat = sat;
+		this.minut = minut;
+	}
+
+	public int getSat() {
+		return sat;
+	}
+
+	public int getMinut() {
+		return minut;
+	}
+
+	@Override
+	public int hashCode() {
+		return 60 * sat + minut;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
+		if (object == null) {
+			return false;
+		}
+		if (getClass() != object.getClass()) {
+			return false;
+		}
+		Vreme that = (Vreme) object;
+		if (this.sat != that.sat) {
+			return false;
+		}
+		if (this.minut != that.minut) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public int compareTo(Vreme that) {
+		return 60 * this.sat + this.minut - 60 * that.sat - that.minut;
+	}
+
+	@Override
+	public String toString() {
+		return (sat < 10 ? "0" : "") + sat + "." + (minut < 10 ? "0" : "") + minut;
+	}
+}
