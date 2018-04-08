4ed10c95e40db06582bf4e47940e13b714a574f3
Ivan Pribela <ivanpribela@gmail.com>
2018-04-08 21:32:16 +0200

    Raspored, Preuzimanje kalendara sa interneta

--- src/org/svetovid/raspored/io/Kalendar.java
+++ src/org/svetovid/raspored/io/Kalendar.java
@@ -1,8 +1,15 @@
 package org.svetovid.raspored.io;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.svetovid.raspored.util.Dnevnik;
+import org.svetovid.raspored.util.Proveri;
 
 /**
  * Ova klasa sluzi za predstavljanje jednog kalendara iz kojeg se uzimaju podaci
@@ -32,5 +39,20 @@ public final class Kalendar {
 
 	public Path getPutanja() {
 		return putanja;
+	}
+
+	public void preuzmi(Path folder) throws IOException {
+		Proveri.argument(folder != null, "folder", folder);
+		Path fajl = folder.resolve(putanja);
+		Dnevnik.trag("Preuzimanje kalendara \"%s\" u fajl \"%s\" sa adrese \"%s\"", ime, fajl, url);
+		try {
+			try (InputStream in = url.openStream()) {
+				Files.copy(in, fajl, StandardCopyOption.REPLACE_EXISTING);
+			}
+			Dnevnik.info("Kalendar \"%s\" je preuzet", ime);
+		} catch (IOException e) {
+			Dnevnik.upozorenje("Kalendar \"%s\" nije je preuzet", e, ime);
+			throw e;
+		}
 	}
 }
