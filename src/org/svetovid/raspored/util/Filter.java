a5c20ca8f8de1e3b413597289cf628642aedfdb0
Ivan Pribela <ivanpribela@gmail.com>
2018-04-13 21:29:00 +0200

    Podrška, Implementacija alternativnog string filtera

--- src/org/svetovid/raspored/util/Filter.java
+++ src/org/svetovid/raspored/util/Filter.java
@@ -7,6 +7,18 @@ import org.svetovid.raspored.model.Cas;
 
 public abstract class Filter {
 
+	public static Predicate<Cas> stringJednak(String izraz, Function<Cas, String> funkcija) throws IllegalArgumentException {
+		Proveri.argument(izraz != null, "izraz", izraz);
+		Proveri.argument(funkcija != null, "funkcija", funkcija);
+		return (Cas cas) -> {
+			String vrednost = funkcija.apply(cas);
+			if (vrednost == null) {
+				return false;
+			}
+			return vrednost.equalsIgnoreCase(izraz);
+		};
+	}
+
 	public static Predicate<Cas> stringSadrzi(String izraz, Function<Cas, String> funkcija) throws IllegalArgumentException {
 		Proveri.argument(izraz != null, "izraz", izraz);
 		Proveri.argument(funkcija != null, "funkcija", funkcija);
