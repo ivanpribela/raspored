77e8942518f939d6e38b5d393c8c867a84e3b9e6
Ivan Pribela <ivanpribela@gmail.com>
2018-04-13 21:27:57 +0200

    Podrška, Implementacija string filtera

---
+++ src/org/svetovid/raspored/util/Filter.java
@@ -0,0 +1,21 @@
+package org.svetovid.raspored.util;
+
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+import org.svetovid.raspored.model.Cas;
+
+public abstract class Filter {
+
+	public static Predicate<Cas> stringSadrzi(String izraz, Function<Cas, String> funkcija) throws IllegalArgumentException {
+		Proveri.argument(izraz != null, "izraz", izraz);
+		Proveri.argument(funkcija != null, "funkcija", funkcija);
+		return (Cas cas) -> {
+			String vrednost = funkcija.apply(cas);
+			if (vrednost == null) {
+				return false;
+			}
+			return vrednost.toUpperCase().contains(izraz.toUpperCase());
+		};
+	}
+}
