593f1bc949f579fef8d8b8679a5e531f95d7b435
Ivan Pribela <ivanpribela@gmail.com>
2018-04-17 21:43:54 +0200

    Program, Čuvanje opcija za filtriranje

--- src/org/svetovid/raspored/cmd/Opcije.java
+++ src/org/svetovid/raspored/cmd/Opcije.java
@@ -1,8 +1,17 @@
 package org.svetovid.raspored.cmd;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+
+import org.svetovid.raspored.model.Cas;
+
 public class Opcije {
 
 	public Opcije() {
+		filteri = new LinkedHashMap<>();
 	}
 
 	////////////
@@ -21,6 +30,14 @@ public class Opcije {
 	// Filteri //
 	/////////////
 
-	// TODO Dodati cuvanje opcija za filtriranje
+	protected final Map<String, List<Predicate<Cas>>> filteri;
 
+	public void addFilter(String kljuc, Predicate<Cas> filter) {
+		List<Predicate<Cas>> lista = filteri.get(kljuc);
+		if (lista == null) {
+			lista = new ArrayList<>();
+			filteri.put(kljuc, lista);
+		}
+		lista.add(filter);
+	}
 }
