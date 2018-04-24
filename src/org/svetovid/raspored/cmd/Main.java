073be16c57bb7b64187068c031b4bf7ab0ab4b1c
Ivan Pribela <ivanpribela@gmail.com>
2018-04-24 21:43:51 +0200

    Licenca, Apache 2.0

--- src/org/svetovid/raspored/cmd/Main.java
+++ src/org/svetovid/raspored/cmd/Main.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 Ivan Pribela
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.svetovid.raspored.cmd;
 
 import java.io.IOException;
@@ -33,11 +49,11 @@ public class Main {
 		Dan dan = null;
 		for (Cas cas : casovi) {
 			if (dan != cas.getDan()) {
-				System.out.println();
-			}
+					System.out.println();
+				}
 			System.out.printf("%10s   %s %s-%s   %-50s %-8s %-30s %-15s %s   %s%n", cas.getStudenti(), cas.getDan().getOznaka(), cas.getVremeOd(), cas.getVremeDo(), cas.getPredmet(), Tip.pretvoriUOznake(cas.getTipovi()), cas.getNastavnik(), cas.getSala(), cas.getId(), cas.getDatumIzmene());
 			dan = cas.getDan();
-		}
+			}
 
 	}
 
